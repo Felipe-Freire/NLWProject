6a9aaf0d9693fa448f43ff4d827e6a5d9fb351f3
Felipe Freire <felipe.guimaraes.freire@gmail.com>
2021-06-25 13:56:29 -0300

    Final Version

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response, NextFunction } from "express";
+import "reflect-metadata";
+import "./database";
+import "express-async-errors";
+import cors from "cors";
+
+import { router } from "./routes";
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  }
+);
+
+// http://localhost:3010
+app.listen(3010, () => console.log("Server is running"));
